520d403f8abd0dc0c81a0a3a0343c5da7f1ba096
sean807H <https://github.com/sean807H>
2025-03-19 10:09:13 +0900

    express 코드로 변환

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-// http 모듈을 불러옵니다.
-const http = require('http');
+// // http 모듈을 불러옵니다.
+// const http = require('http');
 
-// 서버를 생성합니다.
-const server = http.createServer((req, res) => {    // 함수안에 함수 = 콜백함수(CallBack) - 어떤 이벤트가 발생한 후, 수행될 함수를 의미
+// // 서버를 생성합니다.
+// const server = http.createServer((req, res) => {    // 함수안에 함수 = 콜백함수(CallBack) - 어떤 이벤트가 발생한 후, 수행될 함수를 의미
 
-// HTTP 헤더 설정
-res.writeHead(200, { 'Content-Type': 'text/plain' });   // 200대 성공/MIME
+// // HTTP 헤더 설정
+// res.writeHead(200, { 'Content-Type': 'text/plain' });   // 200대 성공/MIME
 
 
 
-// 응답 본문을 보냅니다.
-res.end('Hello, World!\n');
-});
+// // 응답 본문을 보냅니다.
+// res.end('Hello, World!\n');
+// });
 
-// 기존 8080 포트를 3000으로 변경
-server.listen(3000, () => {
-    console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
-  });
+// // 기존 8080 포트를 3000으로 변경
+// server.listen(3000, () => {
+//     console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
+//   });
   
 // Domain Name Service
 
